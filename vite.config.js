affb8b8b31dc9c98c799aa3ff728d14a8febde4c
Oliver Tigges <oliver.tigges@inspired.consulting>
2023-09-28 21:09:57 +0200

    Add privacy page

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,8 @@ export default {
       input: {
         main: new URL('./src/index.html', import.meta.url).pathname,
         imprint: new URL('./src/imprint.html', import.meta.url).pathname,
-        agb: new URL('./src/agb.html', import.meta.url).pathname
+        agb: new URL('./src/agb.html', import.meta.url).pathname,
+        privacy: new URL('./src/privacy.html', import.meta.url).pathname
       }
     }
 
