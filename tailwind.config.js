159ed5fe3b7f60482a1d56d814aaffcf55ddd6ad
Oliver Tigges <oliver.tigges@inspired.consulting>
2023-09-28 14:03:15 +0200

    Added video

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,8 +27,16 @@ module.exports = {
       'sans': ['system-ui', 'ui-sans-serif', 'Roboto', "Helvetica Neue", 'sans-serif'],
       'serif': ['ui-serif', 'Georgia'],
       'mono': ['ui-monospace', 'SFMono-Regular'],
-    }
-  
+    },    
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },  
   },
   plugins: [],
 }
