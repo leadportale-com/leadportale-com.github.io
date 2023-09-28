b52973b286d6de97ac7387cebf1c36eee804cea0
Oliver Tigges <oliver.tigges@inspired.consulting>
2023-09-28 19:33:53 +0200

    Improved main page and separated imprint and AGB

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,6 +32,7 @@ module.exports = {
       padding: {
         DEFAULT: '1rem',
         sm: '2rem',
+        md: '2rem',
         lg: '4rem',
         xl: '5rem',
         '2xl': '6rem',
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,15 @@ const path = require('path')
 export default {
   root: path.resolve(__dirname, 'src'),
   build: {
-    outDir: '..'
+    outDir: '..',
+    rollupOptions: {
+      input: {
+        main: new URL('./src/index.html', import.meta.url).pathname,
+        imprint: new URL('./src/imprint.html', import.meta.url).pathname,
+        agb: new URL('./src/agb.html', import.meta.url).pathname
+      }
+    }
+
   },
   server: {
     port: 8000
