06b71b9c411885ba85264b98a2c47b087c77c3b0
Oliver Tigges <oliver.tigges@inspired.consulting>
2023-10-05 21:14:10 +0200

    Added lottie animation again

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,28 @@
-import '../css/main.css';
+import "../css/main.css";
+import animate from "./components/animation";
 
 function initScrollToTopButton() {
-    let top_area = document.getElementById("page-top-area");
-    let button = document.getElementById("scroll-to-top");
-    button.addEventListener("click", function() {
-        window.scrollTo({top: 0, left: 0, behavior: "smooth"});
-    });    
-    let observer = new IntersectionObserver(function(entries) {
-        entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-                button.classList.add("hidden");
-            } else {
-                button.classList.remove("hidden");
-            }
-          });    
-    }, {threshold: 0.1});
-    observer.observe(top_area);    
+  let top_area = document.getElementById("page-top-area");
+  let button = document.getElementById("scroll-to-top");
+  button.addEventListener("click", function () {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  });
+  let observer = new IntersectionObserver(
+    function (entries) {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          button.classList.add("hidden");
+        } else {
+          button.classList.remove("hidden");
+        }
+      });
+    },
+    { threshold: 0.1 }
+  );
+  observer.observe(top_area);
 }
 
-document.addEventListener("DOMContentLoaded", function() {    
-    initScrollToTopButton();
+document.addEventListener("DOMContentLoaded", function () {
+  initScrollToTopButton();
+  animate(document.getElementById("lottie"));
 });
