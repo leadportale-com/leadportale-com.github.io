a45ba1b0e628ca8e0b1c17b3f1cffca2425a40c5
Oliver Tigges <oliver.tigges@inspired.consulting>
2023-10-05 22:12:26 +0200

    Extracted common nav bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,44 +1,46 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,html}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,html,hbs}"],
   theme: {
     extend: {
       colors: {
-        'blue': '#236aa1',
-        'green': {
-          '50': '#f5faeb',
-          '100': '#e6f5d2',
-          '200': '#d0ebab',
-          '300': '#b1dc7a',
-          '400': '#89c63f',
-          '500': '#75b032',
-          '600': '#598c24',
-          '700': '#466b20',
-          '800': '#39561e',
-          '900': '#32491e',
-          '950': '#18280b',      
+        blue: "#236aa1",
+        green: {
+          50: "#f5faeb",
+          100: "#e6f5d2",
+          200: "#d0ebab",
+          300: "#b1dc7a",
+          400: "#89c63f",
+          500: "#75b032",
+          600: "#598c24",
+          700: "#466b20",
+          800: "#39561e",
+          900: "#32491e",
+          950: "#18280b",
         },
-      },  
-    },  
+      },
+    },
     fontFamily: {
-      'sans': ['system-ui', 'ui-sans-serif', 'Roboto', "Helvetica Neue", 'sans-serif'],
-      'serif': ['ui-serif', 'Georgia'],
-      'mono': ['ui-monospace', 'SFMono-Regular'],
-    },    
+      sans: [
+        "system-ui",
+        "ui-sans-serif",
+        "Roboto",
+        "Helvetica Neue",
+        "sans-serif",
+      ],
+      serif: ["ui-serif", "Georgia"],
+      mono: ["ui-monospace", "SFMono-Regular"],
+    },
     container: {
       padding: {
-        DEFAULT: '1rem',
-        sm: '2rem',
-        md: '2rem',
-        lg: '4rem',
-        xl: '5rem',
-        '2xl': '6rem',
+        DEFAULT: "1rem",
+        sm: "2rem",
+        md: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
       },
-    },  
+    },
   },
   plugins: [],
-}
-
+};
